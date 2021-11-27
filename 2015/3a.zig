1efc5f1efd5e5fdab3cff708d5b13f5b8ae19530
Shritesh Bhattarai <shr@ite.sh>
2021-11-26 23:14:26 -0500

    3a

---
+++ 2015/3a.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+const stdin = std.io.getStdIn().reader();
+
+pub fn main() !void {
+    var visited = std.AutoHashMap([2]i32, void).init(std.heap.page_allocator);
+    defer visited.deinit();
+
+    var coordinate = [_]i32{ 0, 0 };
+    try visited.put(coordinate, {});
+
+    while (stdin.readByte()) |c| {
+        switch (c) {
+            '>' => coordinate[0] += 1,
+            '<' => coordinate[0] -= 1,
+            '^' => coordinate[1] -= 1,
+            'v' => coordinate[1] += 1,
+            else => {},
+        }
+        try visited.put(coordinate, {});
+    } else |_| {}
+
+    std.debug.print("{d}\n", .{visited.count()});
+}
