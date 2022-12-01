fb38eb53ed148bec77d3ff1bc0b5ab201b44a881
Shritesh Bhattarai <shr@ite.sh>
2022-11-30 21:41:53 -0800

    here we go again

---
+++ 2022/src/bin/01/main.rs
@@ -0,0 +1,21 @@
+static INPUT: &str = if cfg!(debug_assertions) {
+    include_str!("example.txt")
+} else {
+    include_str!("input.txt")
+};
+
+fn main() {
+    let mut calories = INPUT
+        .split("\n\n")
+        .map(|group| {
+            group
+                .split('\n')
+                .map(|line| line.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect::<Vec<u32>>();
+    calories.sort_by(|a, b| b.cmp(a)); // descending sort
+
+    println!("Part 1: {}", calories[0]);
+    println!("Part 2: {}", calories[0] + calories[1] + calories[2]);
+}
